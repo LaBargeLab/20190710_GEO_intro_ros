0e9ca812318d39279837637b16c87f4f78c6687d
stefanCOH <stefanhinz19@gmail.com>
2019-07-10 10:29:01 -0700

    upadte

--- script.R
+++ script.R
@@ -14,3 +14,8 @@ gse <- getGEO("GSE102088")
 
 # https://www.ncbi.nlm.nih.gov/geo/
 # https://www.ncbi.nlm.nih.gov/geo/summary/
+# Have a look at the deposition information
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE102088
+# good place to find information about the used protocol/platform/technology
+# Platforms (1)	
+# GPL17586	[HTA-2_0] Affymetrix Human Transcriptome Array 2.0 [transcript (gene) version]
