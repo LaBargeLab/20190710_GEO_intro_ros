1490b835808398a42c28e1deb448e875ccd79c0b
stefanCOH <stefanhinz19@gmail.com>
2019-07-10 10:19:59 -0700

    update

--- script.R
+++ script.R
@@ -3,3 +3,10 @@ BiocManager::install("GEOquery")
 BiocManager::install("affycoretools")
 BiocManager::install("pd.hta.2.0")
 BiocManager::install("reshape2 ")
+
+###############################################
+#################Load Data####################
+##############################################
+
+library(GEOquery)
+gse <- getGEO("GSE102088")
